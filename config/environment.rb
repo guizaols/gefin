6ea6a3e68db91775e7e44bc6e1617a9b286233c7
Guilherme <guilherme@devconnit.com>
2013-05-08 14:53:43 -0300

    Ignore

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 #
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
- 
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   #  config.gem 'rack'
   #  config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
@@ -40,7 +40,7 @@ Rails::Initializer.run do |config|
     #config.gem 'dbd-odbc', :version => '0.2.4', :lib => 'dbd/ODBC'
     #config.gem 'rails-sqlserver-2000-2005-adapter', :source => 'http://gems.github.com'
   end
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -60,7 +60,7 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_modelo_session',
@@ -94,7 +94,7 @@ require 'brazilian-rails'
 require 'brI18n'
 require '_libs'
 require 'prawn/core'
-require 'prawn/labels'
+#require 'prawn/labels'
 require 'brcobranca'
 require 'audit_plus'
 
